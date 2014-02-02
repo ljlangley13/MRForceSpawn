6fad819866851f47bfa3b635cbedef0a55e8fcbe
Luke <ljlangley13@gmai.com>
2014-02-02 01:38:56 +0000

    First Commit
    This is the first commit to Github.

---
+++ readme.txt
@@ -0,0 +1,8 @@
+Welcome to the MRForceSpawn GitHub page.
+
+This is a very simple Minecraft Bukkit plugin I made which teleports players to spawn when they join your server.
+
+More information and answers to your questions can be found on the Bukkit Dev Page: http://dev.bukkit.org/bukkit-plugins/mrforcespawn/
+
+Thank you,
+Luke
---
+++ src/com/mushroomrevival/mrforcespawn/MRForceSpawn.java
@@ -0,0 +1,39 @@
+package com.mushroomrevival.mrforcespawn;
+
+import java.io.File;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MRForceSpawn extends JavaPlugin implements Listener {
+
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+
+		File configFile = new File(this.getDataFolder().getAbsolutePath(),
+				"config.yml");
+		if (configFile.exists() == false) {
+			configFile.mkdir();
+			saveDefaultConfig();
+		}
+	}
+
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onJoin(PlayerJoinEvent event) {
+
+		final Player player = event.getPlayer();
+
+		player.sendMessage(ChatColor.GOLD + "Welcome to "
+				+ (getConfig().getString("ServerName")) + ".");
+
+		event.getPlayer().teleport(
+				getServer().getWorlds().get(0).getSpawnLocation());
+
+	}
+
+}
