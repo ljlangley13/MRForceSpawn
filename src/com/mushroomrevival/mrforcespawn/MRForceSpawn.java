5157b1e2bf04d3995223cdf2ccf7a6aa51531526
Luke <ljlangley13@gmai.com>
2014-06-07 23:43:00 +0100

    Version 1.1 Update
    1. Updated for CB 1.7.9-R0.1
    2. Added a configurable option to turn the join message on and off.

--- src/com/mushroomrevival/mrforcespawn/MRForceSpawn.java
+++ src/com/mushroomrevival/mrforcespawn/MRForceSpawn.java
@@ -5,14 +5,15 @@ import java.io.File;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MRForceSpawn extends JavaPlugin implements Listener {
+	
 
 	public void onEnable() {
+		
 		getServer().getPluginManager().registerEvents(this, this);
 
 		File configFile = new File(this.getDataFolder().getAbsolutePath(),
@@ -23,17 +24,20 @@ public class MRForceSpawn extends JavaPlugin implements Listener {
 		}
 	}
 
-	@EventHandler(priority = EventPriority.NORMAL)
-	public void onJoin(PlayerJoinEvent event) {
+	@EventHandler
+	public void onPlayerJoinEvent(PlayerJoinEvent event) {
 
 		final Player player = event.getPlayer();
+		if (getConfig().getBoolean("Enable-JoinMessage") == true) {
+			player.sendMessage(ChatColor.GOLD + "Welcome to "
+					+ (getConfig().getString("ServerName")) + ".");
+		}
+			event.getPlayer().teleport(
+					getServer().getWorlds().get(0).getSpawnLocation());
 
-		player.sendMessage(ChatColor.GOLD + "Welcome to "
-				+ (getConfig().getString("ServerName")) + ".");
-
-		event.getPlayer().teleport(
-				getServer().getWorlds().get(0).getSpawnLocation());
-
-	}
+		
+			
+		}
+	
 
 }
